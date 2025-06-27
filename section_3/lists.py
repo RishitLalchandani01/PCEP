6a6cb9c6778ddd99c76e0f010b6553ff104e5224
RishitLalchandani01 <rishilalchandani14@gmail.com>
2025-06-27 16:10:34 -0400

    Update lists.py

--- section_3/lists.py
+++ section_3/lists.py
@@ -1,1 +1,20 @@
+# Ideally All lists elements should be of same type
+grocery_list = ['apple','bread','cinnamon','donuts']
+
+
+# lists are ordered set of elements 
+print(grocery_list[0]) # note starts with 0
+print(grocery_list[1]) # note starts with 1
+print(grocery_list[4]) # error
+print(grocery_list[-1]) # error
+
+
+
+top_cities = ['New York City', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix']
+  
+# technically nothing is stopping them to be of different types 
+
+# lists can be empty 
+
+
 
