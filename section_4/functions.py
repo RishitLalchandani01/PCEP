46c3e6fc10b7eaf2da0cfe748efc19447e45725f
RishitLalchandani01 <rishilalchandani14@gmail.com>
2025-06-28 08:25:34 -0400

    Update functions.py

--- section_4/functions.py
+++ section_4/functions.py
@@ -1,1 +1,17 @@
+# functions
+# we have seen "in built" functions like print() and len() and input()
 
+
+# what are functions ?
+# they are seperate units of code that perform a specific task ... they are reusable.
+
+# greet() # NameError: name 'greet' is not defined
+
+def greet():
+    print('Hello World!')
+
+def greet_indian():
+  print('Namaste Duniya!')
+  
+greet()
+greet_indian()
