b8f519a4d7fd489cc5f672fcb132da2071c13865
RishitLalchandani01 <rishilalchandani14@gmail.com>
2025-06-28 09:32:31 -0400

    Update functions.py

--- section_4/functions.py
+++ section_4/functions.py
@@ -94,4 +94,12 @@ print(is_first_last_equal([]))
 
 ### SCOPE NAME #### 
 # scope = the lines in code where variable can properly be recogized and used
+def show_truth():
+  global mysterious_var # >>NOTE<<
+  mysterious_var = 'New Surprise!'
+  print(mysterious_var)
 
+mysterious_var = 'Surprise!'
+print(mysterious_var)
+show_truth() # if function ends all the variables in it are destroyed ... unless global
+print(mysterious_var)
