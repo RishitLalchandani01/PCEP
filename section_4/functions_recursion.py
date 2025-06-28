1629d4f212fb773f9d2efc2710a898bdd5e97d2d
RishitLalchandani01 <rishilalchandani14@gmail.com>
2025-06-28 14:12:54 -0400

    Create functions_recursion.py

---
+++ section_4/functions_recursion.py
@@ -0,0 +1,20 @@
+'''
+def SayHiInfinite(i):
+  print("hi ",i)
+  i+=1
+  SayHiInfinite(i)
+
+j=0
+SayHiInfinite(j)
+''' 
+def SayHi10Times(i):
+  SayHi10Times(i)
+  if i<10:
+    print("hi ",i)
+    i+=1
+  else :
+    print("done")
+    return
+
+SayHi10Times(0)
+print("end of program")
