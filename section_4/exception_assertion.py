15cf6de828498cccf23848809d0c3294437faa28
RishitLalchandani01 <rishilalchandani14@gmail.com>
2025-06-29 12:18:14 -0400

    Update exception_assertion.py

--- section_4/exception_assertion.py
+++ section_4/exception_assertion.py
@@ -1,1 +1,11 @@
-print('hello')
+# there are two ways to raise exception by ourselves, in PCEP you only need to know the first one "AssertionError"
+
+ # Assertions are certain assumptions that we make in our code, and if they are wrong, we want to raise an error
+# https://www.geeksforgeeks.org/python/python-assertion-error/ 
+def calculate_inverse(number):
+  assert (number != 0), 'Got 0 as number!'
+  return 1/number
+
+print(calculate_inverse(5)) # ok  
+print ("\nnext\n")
+calculate_inverse(0) # will raise AssertionError
