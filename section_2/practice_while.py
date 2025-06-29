5e215083b62a388a927c9581c2b493edc4869996
RishitLalchandani01 <rishilalchandani14@gmail.com>
2025-06-29 17:16:59 -0400

    Update practice_while.py

--- section_2/practice_while.py
+++ section_2/practice_while.py
@@ -30,6 +30,30 @@ number = 0
 while number < 10:
   print (number)
   number += 2
-'''
 
+counter = 1
+while counter < 11:
+    print(counter)
+    counter += 1
+print('Finished!')
+
+# note how it prints number from only 1-10 and DOESN'T INCLUDE 11 because it is < 11 same as <= 10
+increasing by 1 speciically is callled incementation
+
+good example: Secret  number game
+'''
+secret_number = 14
+print('''
+==========================
+=== SECRET NUMBER GAME ===
+==========================
+''')
+user_input = int(input('Try to guess the secret number from 0 to 20: '))
+while user_input != secret_number:
+    print('Wrong!')
+    user_input = int(input('Try to guess the secret number from 0 to 20: '))
+print('Perfect! You have guessed the secret number.')
 print ("end of program")
+
+
+NOTICE HOW ''' is used to comment and duriing the print command to print multiple lines at once instead of using \n over and over again
