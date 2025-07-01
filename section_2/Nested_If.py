3cf9c56b730a4127a55b6517497a5e41de98fa9f
RishitLalchandani01 <rishilalchandani14@gmail.com>
2025-07-01 12:52:24 -0400

    Update Nested_If.py

--- section_2/Nested_If.py
+++ section_2/Nested_If.py
@@ -9,3 +9,32 @@ else:
     print('Sorry to hear that!')
 
 # keep in mind how indentation is key
+
+
+#rishit's example:
+birthday_list=[]
+try:
+  month=int(input("What is your birthday month (1-12): "))
+  if month == 1:
+    birthday_list.append(month)
+    print("Me too!")
+    day=int(input("What is your birthday day (1-31): "))
+    if day==14:
+      birthday_list.append(day)
+      print("Me too!")
+      year=int(input("What is your birthday year (1800-2025): "))
+      if year==2010:
+        birthday_list.append(year)
+        print("Me too!")
+        print("Looks like we have the same birthday!")
+        print(birthday_list)
+      else:
+        print("look's like we don't have the same birthday!")
+    else:
+      print("look's like we don't have the same birthday!")
+  else:
+    print("look's like we don't have the same birthday!")
+  
+  print("end of program!")
+except ValueError:
+  print("Provide a number next time, bye!")
