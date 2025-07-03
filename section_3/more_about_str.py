2d4d42da9a631ee169a04128de328d4039bc28a6
RishitLalchandani01 <rishilalchandani14@gmail.com>
2025-07-02 21:54:19 -0400

    Create more_about_str.py

---
+++ section_3/more_about_str.py
@@ -0,0 +1,16 @@
+def str_1():
+  fav_band = 'Green#Day'
+  print(fav_band[6])
+
+def str_2():
+  fav_band = 'Green#Day'
+  print(fav_band[:6])
+
+def str_3():
+  fav_band = 'Green#Day'
+  #fav_band[6] = 'M' # TypeError: 'str' object does not support item assignment
+
+# NOTE : strings are immutable 
+# it has methods like upper() lower() etc.
+
+str_3()
