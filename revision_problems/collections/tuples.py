fc20a29ff4c65cc499c7815f7496b496857ffd89
RishitLalchandani01 <rishilalchandani14@gmail.com>
2025-07-04 11:18:34 -0400

    Create tuples.py

---
+++ revision_problems/collections/tuples.py
@@ -0,0 +1,8 @@
+def concept_1():
+  t = ("sandy", "mandy", "candy","andy")
+  print(sorted(t)) 
+  ############
+  # ['andy', 'candy', 'mandy', 'sandy']
+  # The sorted() function will sort the tuple in ascending order 
+  # BUT the output of the sorted function is a list.
+  
